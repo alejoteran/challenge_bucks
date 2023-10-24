b163437369a7ad2e502d4fb91490e5b8765d7bf6
Juando26030 <juando02603.com>
2023-10-23 22:29:26 -0500

    mi primer modal :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbarra from "./components/navbar.jsx";
+import ModalJuanda from "./components/modalJuanda.jsx";
 import React from "react";
 
 function App() {
   return (
       <>
           <Navbarra />
+          <ModalJuanda />
       </>
   );
 }
